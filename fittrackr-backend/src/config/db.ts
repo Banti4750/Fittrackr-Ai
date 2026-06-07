b1bf8cff6ba9675aa3e1522ab7ce54db6a3750ea
Banti4750 <bantikumar6203818460@gmail.com>
2026-06-07 21:20:09 +0530

    chore: comment out database connection log for cleaner output

--- fittrackr-backend/src/config/db.ts
+++ fittrackr-backend/src/config/db.ts
@@ -4,7 +4,7 @@ import { env } from './env';
 export async function connectDB(): Promise<void> {
   mongoose.set('strictQuery', true);
   await mongoose.connect(env.mongoUri);
-  console.log(`[db] connected: ${mongoose.connection.host}/${mongoose.connection.name}`);
+  // console.log(`[db] connected: ${mongoose.connection.host}/${mongoose.connection.name}`);
 
   mongoose.connection.on('error', (err) => console.error('[db] error:', err));
   mongoose.connection.on('disconnected', () => console.warn('[db] disconnected'));
